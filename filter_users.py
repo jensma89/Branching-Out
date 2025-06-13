bbc5893e3e4997257a763df0b91206ad1e7129b8
Jens M. <jensmayer89@gmail.com>
2025-06-13 15:44:13 +0200

    Create filter by e-mail feature

--- filter_users.py
+++ filter_users.py
@@ -7,6 +7,16 @@ def get_user_data(file_path):
         return json.load(file)
 
 
+def filter_by_email(email):
+    """Filter user data by e-mail address."""
+    users = get_user_data("users.json")
+    filtered_users_by_email = [user for user in users
+                               if user['email'].lower() == email.lower()]
+
+    for user in filtered_users_by_email:
+        print(user)
+
+
 def filter_by_age(age):
     """Filter the user data by age."""
     users = get_user_data("users.json")
@@ -28,15 +38,21 @@ def filter_users_by_name(name):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently,"
-                          " only 'name' and 'age' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? "
+                          "('name', 'age' or by 'e-mail'): ").strip().lower()
 
     if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
+        name_to_search = input("Enter a name "
+                               "to filter users: ").strip()
         filter_users_by_name(name_to_search)
     elif filter_option == "age":
-        age_to_search = int(input("Enter an age to filter users: ").strip())
+        age_to_search = int(input("Enter an age "
+                                  "to filter users: ").strip())
         filter_by_age(age_to_search)
+    elif filter_option == "e-mail":
+        email_to_search = input("Enter an e-mail address "
+                                "to filter users: ").strip()
+        filter_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
 
